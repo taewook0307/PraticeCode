b82c342fa42db770e92e691a1b695ea2061488d3
Kim Tae Wook <gmill0411@naver.com>
2024-08-14 20:53:44 +0900

    실패율 정렬 조건 수정

--- 240814_42889/240814_42889.cpp
+++ 240814_42889/240814_42889.cpp
@@ -31,7 +31,7 @@
 
 bool Compare(const std::pair<float, int>& _LeftPair, const std::pair<float, int>& _RightPair)
 {
-    if (_LeftPair.first == _RightPair.first)
+    if (fabs(_LeftPair.first - _RightPair.first) < 0.00000000001f)
     {
         return _LeftPair.second < _RightPair.second;
     }
