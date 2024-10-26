b1dfe18bf2af63c780046a8c000ae57b2953c32a
Kim Tae Wook <gmill0411@naver.com>
2024-10-26 15:56:39 +0900

    Compare_The_Triplets

---
+++ 241026_Compare_The_Triplets/241026_Compare_The_Triplets.cpp
@@ -0,0 +1,35 @@
+﻿// 241026_Compare_The_Triplets.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+std::vector<int> compareTriplets(std::vector<int> a, std::vector<int> b)
+{
+	std::vector<int> Result(2, 0);
+
+    const size_t Count = a.size();
+
+	for (size_t i = 0; i < Count; ++i)
+	{
+		if (a[i] > b[i])
+		{
+			++Result[0];
+		}
+		else if (a[i] < b[i])
+		{
+			++Result[1];
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	return Result;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
