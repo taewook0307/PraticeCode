129ddd78ec7140962ec2f252f269ac0c51f52f2e
Kim Tae Wook <gmill0411@naver.com>
2024-10-26 15:48:24 +0900

    Simple_Array_Sum

---
+++ 241026_Simple_Array_Sum/241026_Simple_Array_Sum.cpp
@@ -0,0 +1,16 @@
+﻿// 241026_Simple_Array_Sum.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+int simpleArraySum(std::vector<int> ar)
+{
+    return std::accumulate(ar.begin(), ar.end(), 0);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
